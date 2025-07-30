8db563f27911bcfe82bb74a18acc10fced40ef3c
chief.lee <64768242+CBike@users.noreply.github.com>
2025-07-30 18:37:41 +0900

    Changes

---
+++ The-Leaky-Intergrate-and-Fire-Neuron-Model/1.py
@@ -0,0 +1,9 @@
+import snntorch as snn
+from snntorch import spikeplot as splt
+from snntorch import spikegen
+
+import torch
+import torch.nn as nn
+
+import numpy as np
+import matplotlib.pyplot as pl
