0faaf984bcb0edfa6686fd603fcbd5d0fd04d341
chief.lee <64768242+CBike@users.noreply.github.com>
2025-08-14 11:57:34 +0900

    Changes

--- Surrogate Gradient Descent in a Convolutional SNN/Surrogate Gradient Descent.py
+++ Surrogate Gradient Descent in a Convolutional SNN/Surrogate Gradient Descent.py
@@ -59,3 +59,171 @@ beta = 0.5
 
 lif1 = snn.Leaky(beta=beta, spike_grad=spike_grad)
 
+# dataloader arguments
+batch_size = 128
+data_path='/tmp/data/mnist'
+
+dtype = torch.float
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
+# Define a transform
+transform = transforms.Compose([
+            transforms.Resize((28, 28)),
+            transforms.Grayscale(),
+            transforms.ToTensor(),
+            transforms.Normalize((0,), (1,))])
+
+mnist_train = datasets.MNIST(data_path, train=True, download=True, transform=transform)
+mnist_test = datasets.MNIST(data_path, train=False, download=True, transform=transform)
+
+# Create DataLoaders
+train_loader = DataLoader(mnist_train, batch_size=batch_size, shuffle=True, drop_last=True)
+test_loader = DataLoader(mnist_test, batch_size=batch_size, shuffle=True, drop_last=True)
+# neuron and simulation parameters
+spike_grad = surrogate.fast_sigmoid(slope=25)
+beta = 0.5
+num_steps = 50
+
+# Define Network
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # Initialize layers
+        self.conv1 = nn.Conv2d(1, 12, 5)
+        self.lif1 = snn.Leaky(beta=beta, spike_grad=spike_grad)
+        self.conv2 = nn.Conv2d(12, 64, 5)
+        self.lif2 = snn.Leaky(beta=beta, spike_grad=spike_grad)
+        self.fc1 = nn.Linear(64*4*4, 10)
+        self.lif3 = snn.Leaky(beta=beta, spike_grad=spike_grad)
+
+    def forward(self, x):
+
+        # Initialize hidden states and outputs at t=0
+        mem1 = self.lif1.init_leaky()
+        mem2 = self.lif2.init_leaky()
+        mem3 = self.lif3.init_leaky()
+
+        cur1 = F.max_pool2d(self.conv1(x), 2)
+        spk1, mem1 = self.lif1(cur1, mem1)
+
+        cur2 = F.max_pool2d(self.conv2(spk1), 2)
+        spk2, mem2 = self.lif2(cur2, mem2)
+
+        cur3 = self.fc1(spk2.view(batch_size, -1))
+        spk3, mem3 = self.lif3(cur3, mem3)
+
+        return spk3, mem3
+
+
+#  Initialize Network
+net = nn.Sequential(nn.Conv2d(1, 12, 5),
+                    nn.MaxPool2d(2),
+                    snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True),
+                    nn.Conv2d(12, 64, 5),
+                    nn.MaxPool2d(2),
+                    snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True),
+                    nn.Flatten(),
+                    nn.Linear(64*4*4, 10),
+                    snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True, output=True)
+                    ).to(device)
+
+
+data, targets = next(iter(train_loader))
+data = data.to(device)
+targets = targets.to(device)
+
+for step in range(num_steps):
+    spk_out, mem_out = net(data)
+
+# already imported snntorch.functional as SF
+loss_fn = SF.ce_rate_loss()
+loss_val = loss_fn(spk_rec, targets)
+acc = SF.accuracy_rate(spk_rec, targets)
+
+def batch_accuracy(train_loader, net, num_steps):
+  with torch.no_grad():
+    total = 0
+    acc = 0
+    net.eval()
+
+    train_loader = iter(train_loader)
+    for data, targets in train_loader:
+      data = data.to(device)
+      targets = targets.to(device)
+      spk_rec, _ = forward_pass(net, num_steps, data)
+
+      acc += SF.accuracy_rate(spk_rec, targets) * spk_rec.size(1)
+      total += spk_rec.size(1)
+
+  return acc/total
+
+test_acc = batch_accuracy(test_loader, net, num_steps)
+
+optimizer = torch.optim.Adam(net.parameters(), lr=1e-2, betas=(0.9, 0.999))
+num_epochs = 1
+loss_hist = []
+test_acc_hist = []
+counter = 0
+
+# Outer training loop
+for epoch in range(num_epochs):
+
+    # Training loop
+    for data, targets in iter(train_loader):
+        data = data.to(device)
+        targets = targets.to(device)
+
+        # forward pass
+        net.train()
+        spk_rec, _ = forward_pass(net, num_steps, data)
+
+        # initialize the loss & sum over time
+        loss_val = loss_fn(spk_rec, targets)
+
+        # Gradient calculation + weight update
+        optimizer.zero_grad()
+        loss_val.backward()
+        optimizer.step()
+
+        # Store loss history for future plotting
+        loss_hist.append(loss_val.item())
+
+        # Test set
+        if counter % 50 == 0:
+        with torch.no_grad():
+            net.eval()
+
+            # Test set forward pass
+            test_acc = batch_accuracy(test_loader, net, num_steps)
+            print(f"Iteration {counter}, Test Acc: {test_acc * 100:.2f}%\n")
+            test_acc_hist.append(test_acc.item())
+
+        counter += 1
+
+
+# Plot Loss
+fig = plt.figure(facecolor="w")
+plt.plot(test_acc_hist)
+plt.title("Test Set Accuracy")
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy")
+plt.show()
+
+spk_rec, mem_rec = forward_pass(net, num_steps, data)
+from IPython.display import HTML
+
+idx = 0
+
+fig, ax = plt.subplots(facecolor='w', figsize=(12, 7))
+
+labels=['0', '1', '2', '3', '4', '5', '6', '7', '8','9']
+
+# plt.rcParams['animation.ffmpeg_path'] = 'C:\\path\\to\\your\\ffmpeg.exe'
+
+#  Plot spike count histogram
+anim = splt.spike_count(spk_rec[:, idx].detach().cpu(), fig, ax, labels=labels,
+                        animate=True, interpolate=4)
+
+# HTML(anim.to_html5_video())
+anim.save("spike_bar.mp4")
+print(f"The target label is: {targets[idx]}")
