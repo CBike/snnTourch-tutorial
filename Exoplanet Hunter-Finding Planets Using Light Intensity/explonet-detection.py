25e61efb820bde962ff530e6ea341139b41d0c84
chief.lee <64768242+CBike@users.noreply.github.com>
2025-08-19 18:18:08 +0900

    Changes

--- Exoplanet Hunter-Finding Planets Using Light Intensity/explonet-detection.py
+++ Exoplanet Hunter-Finding Planets Using Light Intensity/explonet-detection.py
@@ -1,4 +1,5 @@
 import snntorch as snn
+from pyexpat import features
 from snntorch import surrogate
 
 
@@ -28,7 +29,37 @@ from plotly.subplots import make_subplots
 from sklearn.metrics import classification_report
 from sklearn.metrics import  roc_auc_score
 
-#google colab mount
+
+
+class CustomDataset(Dataset):
+    def __init__(self, csv_file, transform=None):
+        with open(csv_file, 'r') as f:
+            self.data = pd.read_csv(f)
+
+        self.labels = self.data.iloc[:,0].values
+        self.features = self.data.iloc[:,1:].values
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        label = self.labels[idx]
+        feature = self.features[idx]
+
+        if self.transform:
+            feature = self.transform(feature)
+
+
+        sample = {'feature': feature, 'label': label}
+
+        return sample
+
+
+
+
+train_dataset = CustomDataset('')
+test_dataset = CustomDataset('')
 
 
 
